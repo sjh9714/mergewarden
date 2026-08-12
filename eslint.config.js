ca6c012784f766188a3a4d90a57de21375d97c2f
sjh9714 <163989462+sjh9714@users.noreply.github.com>
2026-08-12 09:01:51 +0900

    ci: demo/ is a browser-context harness, lint skips it like prettier does

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   {
-    ignores: ["**/dist/**", "**/coverage/**", "**/node_modules/**"],
+    ignores: ["**/dist/**", "**/coverage/**", "**/node_modules/**", "demo/**"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
